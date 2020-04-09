be748c90d20ffebb9c78217999c14ebb251e3d1f
tsubasa111 <tabletennisworks@gmail.com>
2020-04-09 18:53:21 +0900

    経歴を作成しました

--- src/components/card/Works.vue
+++ src/components/card/Works.vue
@@ -1,20 +1,20 @@
 <template>
-  <v-container>
-    <div id="demo">
-      <button v-on:click="show = !show">Toggle</button>
-      <transition name="fade">
-        <p v-if="show">hello</p>
-      </transition>
-    </div>
-  </v-container>
+  <v-card>
+    <v-card-title :class="item.color">
+      <!-- <v-icon dark size="42" class="mr-4">mdi-magnify</v-icon> -->
+      <h2 class="white--text font-weight-light">{{ item.title }}</h2>
+    </v-card-title>
+    <v-card-text class="pt-4 black--text">{{ item.text }}</v-card-text>
+  </v-card>
 </template>
 
 <script>
 export default {
   name: 'cardSkill',
+  props: ['item'],
   data () {
     return {
-      show: false
+
     }
   }
 }
--- src/pages/Works.vue
+++ src/pages/Works.vue
@@ -1,12 +1,52 @@
 <template>
-  <div></div>
+  <v-container>
+    <v-timeline class="timeline mx-auto" :dense="$vuetify.breakpoint.smAndDown">
+      <v-timeline-item v-for="item in items" :key="item.title" :color="item.color">
+        <card v-bind="{item}" />
+      </v-timeline-item>
+    </v-timeline>
+  </v-container>
 </template>
 
 <script>
-export default {
+import Card from '../components/card/Works'
 
+export default {
+  name: 'works',
+  components: {
+    card: Card
+  },
+  data () {
+    return {
+      items: [
+        {
+          title: '採用管理ツールの開発',
+          text: 'マイナビやリクナビの媒体で応募者の進捗状況を共有する機能を制作する社内ツールのプロジェクトです。フロントエンドでアサインし、3ヶ月ほど経過した後に同じプロジェクトでバックエンドに切り替わりました。主にバグの改修が多かったです。',
+          color: 'primary'
+        },
+        {
+          title: 'メディアWebアプリケーション',
+          text: '厳選された人がお店を紹介するメディアサイトを作成するプロジェクトです。このプロジェクトはバックエンドでアサインしました。業務はApiの実装と、管理サイトのcrud機能を追加でした。ベンチャーだったのでほぼ1から実装する箇所がいくつもあり、とても勉強になりました。',
+          color: 'teal lighten-1'
+        },
+        {
+          title: '仲介Webサイト',
+          text: 'お店を紹介するサイトを制作するプロジェクトです。バック兼フロントで実装行い、基本的なタスクは新機能追加と企画物のページ実装です。当時、大規模リニューアルに関わり、実装ペースやタスクの優先度を考えて動く経験しました。',
+          color: 'purple lighten-2'
+        },
+        {
+          title: '案件探し中',
+          text: '案件を探しております。',
+          color: 'orange lighten-1'
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
+.timeline {
+  max-width: 1300px;
+}
 </style>
