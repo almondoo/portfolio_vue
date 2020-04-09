2be29c8f8cd22d70ab9cea7d5dbec5994ca4858e
tsubasa111 <tabletennisworks@gmail.com>
2020-04-09 19:06:55 +0900

    コンタクトページ作成

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,12 +1,32 @@
 <template>
-  <div></div>
+  <v-container>
+    <v-expansion-panels class="mx-auto panel_main" focusable>
+      <v-expansion-panel>
+        <v-expansion-panel-header>{{ item.title }}</v-expansion-panel-header>
+        <a :href="'mailto:' + item.content">
+          <v-expansion-panel-content class="pt-4">{{ item.content }}</v-expansion-panel-content>
+        </a>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'contact'
+  name: 'contact',
+  data () {
+    return {
+      item: {
+        title: 'Contact',
+        content: 'tabletennisworks@gmail.com'
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
+.panel_main {
+  max-width: 500px;
+}
 </style>
