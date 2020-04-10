bf38d47c91d0a8514d170fed827ec01725cf9780
tsubasa111 <tabletennisworks@gmail.com>
2020-04-10 17:19:51 +0900

    修正

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,7 @@
         </v-list-item-avatar>
 
         <v-list-item-content>
-          <v-list-item-title>Tsubasa Morikawa</v-list-item-title>
+          <v-list-item-title>T M</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
 
